7f2f7e97b32b2e67661ba907b7c35f3de6295bfb
mp11006 <98921624+mp11006@users.noreply.github.com>
2022-02-09 19:40:24 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -26,8 +26,8 @@ def webServer(port=13331):
         outputdata = f.read()
         
         #Send one HTTP header line into socket.
-        connectionSocket.send('\HTTP/1.1 200 OK\n\n')
-        connectionSocket.send(outputdata)
+        connectionSocket.send("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n")
+        connectionSocket.send("\r\n")
 
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
